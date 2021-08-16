b53bc845b91f21efe6e7085818b45e73a495712c
kthss01 <kthss01@naver.com>
2021-08-16 17:37:31 +0900

    데이터베이스 커넥션 풀 구축

---
+++ UserChat/src/com/model/dto/UserDto.java
@@ -0,0 +1,16 @@
+package com.model.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+
+	private String userID;
+	private String userPassword;
+	private String userName;
+	private int userAge;
+	private String userGender;
+	private String userEmail;
+	private String userProfile;
+	
+}
