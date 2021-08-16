d9026fc6cdd214d5c075ddc40fc5b3e0ccfbbf33
kthss01 <kthss01@naver.com>
2021-08-16 18:20:21 +0900

    회원가입 DB 기능 구현

---
+++ UserChat/src/com/controller/UserRegisterCheckServlet.java
@@ -0,0 +1,26 @@
+package com.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.model.dao.UserDao;
+
+@WebServlet("/userRegisterCheck.do")
+public class UserRegisterCheckServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		
+		response.setContentType("text/html; charset=UTF-8");
+		
+		String userID = request.getParameter("userID");
+		response.getWriter().write(new UserDao().registerCheck(userID) + "");
+	}
+
+}
