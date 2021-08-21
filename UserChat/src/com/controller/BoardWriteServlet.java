c6d517da09aec0303f746363de270a6138b31215
kthss01 <kthss01@naver.com>
2021-08-21 17:45:26 +0900

    파일 및 게시글 등록 기능 구현

---
+++ UserChat/src/com/controller/BoardWriteServlet.java
@@ -0,0 +1,78 @@
+package com.controller;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.model.dao.BoardDao;
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
+
+public class BoardWriteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		
+		response.setContentType("text/html; charset=UTF-8");
+		
+		MultipartRequest multi = null;
+		
+		int fileMaxSize = 10 * 1024 * 1024;
+		// deprecated됨 
+//		String savePath = request.getRealPath("/upload").replaceAll("\\\\", "/");
+		String savePath = request.getSession().getServletContext().getRealPath("/upload").replaceAll("\\\\", "/"); 
+		try {
+			multi = new MultipartRequest(request, savePath, fileMaxSize, "UTF-8", new DefaultFileRenamePolicy());
+		} catch (Exception e) {
+			request.getSession().setAttribute("messageType", "오류 메시지");
+			request.getSession().setAttribute("messageContent", "파일 크기는 10MB를 넘을 수 없습니다.");
+			response.sendRedirect("boardWrite.jsp");
+			return;
+		}
+		
+		String userID = multi.getParameter("userID");
+		
+		// 현재 건너온 유저와 session 유저가 일치하는지 확인
+		HttpSession session = request.getSession();
+		if (!userID.equals((String) session.getAttribute("userID"))) {
+			session.setAttribute("messageType", "오류 메시지");
+			session.setAttribute("messageContent", "접근 할 수 없습니다.");
+			response.sendRedirect("index.jsp");
+			return;
+		}
+		
+		String boardTitle = multi.getParameter("boardTitle");
+		String boardContent = multi.getParameter("boardContent");
+		
+		if (boardTitle == null || boardTitle.equals("") || boardContent == null || boardContent.equals("")) {
+			request.getSession().setAttribute("messageType", "오류 메시지");
+			request.getSession().setAttribute("messageContent", "내용을 모두 채워주세요.");
+			response.sendRedirect("boardWrite.jsp");
+			return;
+		}
+
+		String boardFile = "";
+		String boardRealFile = "";
+		
+		File file = multi.getFile("boardFile");
+		if (file != null) {
+			boardFile = multi.getOriginalFileName("boardFile");
+			boardRealFile = file.getName();
+		}
+
+		BoardDao boardDao = new BoardDao();
+		boardDao.write(userID, boardTitle, boardContent, boardFile, boardRealFile);
+		
+		session.setAttribute("messageType", "성공 메시지");
+		session.setAttribute("messageContent", "성공적으로 게시물이 작성되었습니다.");
+		response.sendRedirect("boardView.jsp");
+		return;
+	}
+
+}
---
+++ UserChat/src/com/model/dao/BoardDao.java
@@ -0,0 +1,107 @@
+package com.model.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+import com.model.dto.BoardDto;
+
+public class BoardDao {
+
+	DataSource dataSource;
+	
+	public BoardDao() {
+		try {
+			InitialContext initContext = new InitialContext();
+			Context envContext = (Context) initContext.lookup("java:/comp/env");
+			dataSource = (DataSource) envContext.lookup("jdbc/UserChat");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public int write(String userID, String boardTitle, String boardContent, String boardFile, String boardRealFile) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = "INSERT INTO BOARD SELECT ?, IFNULL((SELECT MAX(boardID) + 1 FROM BOARD), 1), ?, ?, now(), 0, ?, ?, IFNULL((SELECT MAX(boardGroup) + 1 FROM BOARD), 0), 0, 0";
+		
+		try {
+			
+			conn = dataSource.getConnection();
+			
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1, userID);
+			pstmt.setString(2, boardTitle);
+			pstmt.setString(3, boardContent);
+			pstmt.setString(4, boardFile);
+			pstmt.setString(5, boardRealFile);
+			
+			return pstmt.executeUpdate();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null) rs.close();
+				if (pstmt != null) pstmt.close();
+				if (conn != null) conn.close();
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		
+		return -1; // 데이터베이스 오류
+	}
+	
+	public BoardDto getBoard(String boardID) {
+		BoardDto board = new BoardDto();
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = "SELECT * FROM BOARD WHERE boardID = ?";
+		
+		try {
+			
+			conn = dataSource.getConnection();
+			
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1, boardID);
+			
+			rs = pstmt.executeQuery();
+			
+			if (rs.next()) {
+				board.setUserID(rs.getString("userID"));
+				board.setBoardID(rs.getInt("boardID"));
+				board.setBoardTitle(rs.getString("boardTitle").replaceAll(" ", "&nbsp;").replaceAll("<", "&lt;").replaceAll(">", "&gt;").replaceAll("\n", "<br>"));
+				board.setBoardContent(rs.getString("boardContent").replaceAll(" ", "&nbsp;").replaceAll("<", "&lt;").replaceAll(">", "&gt;").replaceAll("\n", "<br>"));
+				board.setBoardDate(rs.getString("boardDate").substring(0, 11));
+				board.setBoardHit(rs.getInt("boardHit"));
+				board.setBoardFile(rs.getString("boardFile"));
+				board.setBoardRealFile(rs.getString("boardRealFile"));
+				board.setBoardGroup(rs.getInt("boardGroup"));
+				board.setBoardSequence(rs.getInt("boardSequence"));
+				board.setBoardLevel(rs.getInt("boardLevel"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null) rs.close();
+				if (pstmt != null) pstmt.close();
+				if (conn != null) conn.close();
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		
+		return board;
+	}
+}
